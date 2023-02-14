1fecbba022c8e03acab5dedcbca1d23f9163fcec
Wes Hinsley <w.hinsley@imperial.ac.uk>
2023-02-14 19:15:02 +0000

    Validate contact page

--- contact.php
+++ contact.php
@@ -6,32 +6,33 @@
 
 ?>
 
-        <header class="page-header">
-          <h1>Contact us</h1>
-        </header>
+      <header class="page-header">
+        <h1>Contact us</h1>
+      </header>
 
-        <div class="row-fluid">
-          <div class="span8">
+      <div class="row-fluid">
+        <div class="span8">
   
-            Please address all enquiries about the course to <a href="mailto:infectiousdiseasemodels@imperial.ac.uk">Dr Lilith Whittles and Dr Clare McCormack</a>.
-            <hr/>
+          Please address all enquiries about the course to <a href="mailto:infectiousdiseasemodels@imperial.ac.uk">Dr Lilith Whittles and Dr Clare McCormack</a>.
+          <hr>
       
-            <p><strong>Email :</strong> <a href="mailto:infectiousdiseasemodels@imperial.ac.uk">infectiousdiseasemodels@imperial.ac.uk</a></p>
-            <p>
-              <strong>Post:</strong> 
-              <br />
-              <address>
-                Department of Infectious Disease Epidemology,<br/>
-                Imperial College - St. Mary's Campus,<br />
-                Norfolk Place,<br />
-                London<br/>
-                W2 1PG<br />
-              </address>
-            </p>
-          </div>
-          <div class="span4">
-          </div>
+          <p><strong>Email :</strong> <a href="mailto:infectiousdiseasemodels@imperial.ac.uk">infectiousdiseasemodels@imperial.ac.uk</a></p>
+          <p>
+            <strong>Post:</strong> 
+            <br>
+            <address>
+              Department of Infectious Disease Epidemology,<br>
+              Imperial College - St. Mary's Campus,<br>
+              Norfolk Place,<br>
+              London<br>
+              W2 1PG<br>
+            </address>
+          </p>
         </div>
+        <div class="span4">
+        </div>
+      </div>
+    </div>
 
 <?php 
   include "footer.php"
