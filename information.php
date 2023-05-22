17ce3a767fd6ebbaadc326ad6bf809aa7d6cbcea
Lilith Whittles <lwhittles@users.noreply.github.com>
2023-05-22 17:05:19 +0100

    Update information.php
    deadline passed

--- information.php
+++ information.php
@@ -60,7 +60,7 @@
                 <p>Payments will be accepted until <?=friendly_date($last_payment_date) ?> (5pm BST)</p>
                 <h2>Scholarships available for candidates from LMICs</h2>
 
-            <!--   <p class="alert alert-info"><b>Please Note: </b> The deadline to apply for the LMIC Scholarship has now passed</p> -->
+            <p class="alert alert-info"><b>Please Note: </b> The deadline to apply for the LMIC Scholarship has now passed</p>
 
                 <p>We offer a limited number of scholarships for participants from LMICs; scholarship applications must be received by <?=friendly_date($scholarship_date)?>.</p>
 
