f338d568101c683c9ce09e63cf77d8f15fcb59c9
Thom Rawson <t.rawson@imperial.ac.uk>
2025-01-23 21:50:36 +0000

    Remove in-person course message.

--- index.php
+++ index.php
@@ -30,8 +30,7 @@
 
       <div class="row-fluid">
         <div class="span8">
-          <p class="lead"><span style="color:#800000"><strong><u>IN-PERSON COURSE FOR 2025:</u></strong></span><span style="color:#800000"> We look forward to welcoming delegates in person in 2025, circumstances permitting. If a move to remote teaching is necessary for any reason, we will keep delegates informed regarding logistics and fees.
-          </span></p>
+
 
           <hr>
           <p class="lead"> Taught by leading researchers who advise policy-makers internationally. Topics include HIV, TB, malaria, outbreak response, COVID-19, health economics, vaccination programmes, stochastic models &amp; more.</p>
