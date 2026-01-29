cb315354e8e3ccfb3485f2afed1279f5f8c8e919
pabloperguz <p.perez-guzman@imperial.ac.uk>
2026-01-29 14:55:59 +0000

    2026 update metadata

--- data/metadata.php
+++ data/metadata.php
@@ -9,18 +9,18 @@ $show_timetable = false;
 
 // Start and end dates of course
 
-$start_date = "2025-09-15";
-$end_date = "2025-09-26";
-$maths_date = "2025-09-14";
+$start_date = "2026-09-14";
+$end_date = "2026-09-25";
+$maths_date = "2026-09-13";
 
-$scholarship_date = "2025-03-16";
+$scholarship_date = "2026-03-22";
 $today = date('Y-m-m');
 
-$early_booking_date = "2025-06-01";
+$early_booking_date = "2026-05-31";
 $early_cost = "2565";
 $standard_cost = "2850";
 
-$last_payment_date = "2025-08-31";
+$last_payment_date = "2026-08-31";
 
 $acc_policy_created = "2023-01-16";
 $acc_policy_updated = "2023-01-16";
